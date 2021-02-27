c58c4afb9eac5c86e9602b3f91fcea29fd63d619
md.aamroni <md.aamroni@gmail.com>
2021-02-27 18:22:43 +0600

    Go Project Initialization

--- 01_Hello/main.go
+++ 01_Hello/main.go
@@ -3,5 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Go Lang")
+	fmt.Println(foo())
+	iterator()
+	declared()
+	bar()
+}
+
+func iterator() {
+	for i := 1; i <= 10; i++ {
+		if i%2 == 0 {
+			fmt.Println("Is", i, "a even number?")
+		}
+	}
+}
+
+func bar() {
+	fmt.Println("It's time to exit")
+}
+
+func foo() string {
+	return "Hello Web Application Developer, How are you?"
+}
+
+func declared() {
+	a := 10
+	b := 10
+	c := a + b
+	fmt.Println(a, "+", b, "; The total equation is =", c)
 }
